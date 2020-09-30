5460102334e4139b12b10b0e2aacfb82069b674f
igorlmgx <igorlmgx@gmail.com>
2020-09-29 23:09:31 -0300

    Adicionando profundidade na query de pet

--- api/pet/controllers/pet.js
+++ api/pet/controllers/pet.js
@@ -11,6 +11,22 @@ function random() {
 }
 
 module.exports = {
+    async find(ctx) {
+        let entities;
+        if (ctx.query._q) {
+            entities = await strapi.services.pet.search(ctx.query);
+        } else {
+            entities = await strapi.services.pet.find(ctx.query, ['campus', 'campus.universidade', 'logo']);
+        }
+
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.pet }));
+    },
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.services.pet.findOne({ id }, ['campus', 'campus.universidade', 'logo']);
+        return sanitizeEntity(entity, { model: strapi.models.pet });
+    },
     geojson: async ctx => {
         let entities;
         if (ctx.query._q) {
